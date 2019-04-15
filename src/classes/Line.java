64fcdf84505fa5c3718249770574e71ded664a95
Penguin <naoto121jp@yahoo.co.jp>
2019-04-15 16:54:57 +0900

    add class line

---
+++ src/classes/Line.java
@@ -0,0 +1,12 @@
+package classes;
+import classes.*;
+
+public class Line {
+    Vector P;
+    Vector Q;
+
+    public Line (Vector P, Vector Q) {
+        this.P = P;
+        this.Q = Q;
+    }
+}
