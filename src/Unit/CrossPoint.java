fa6c8a374630b1ac2c3400232833d98951237d48
Penguin <naoto121jp@yahoo.co.jp>
2019-04-16 15:41:17 +0900

    fixed CrossPoint

--- src/Unit/CrossPoint.java
+++ src/Unit/CrossPoint.java
@@ -11,22 +11,20 @@ public class CrossPoint{
   public void ask_for_a(Line P,Line Q)
   {
    int A;
-    A=(P.q.x-P.p.x)*(Q.p.y-Q.q.y)+(Q.q.x-Q.p.x)*(P.q.y-P.p.y)
+    A=(P.q.x-P.p.x)*(Q.p.y-Q.q.y)+(Q.q.x-Q.p.x)*(P.q.y-P.p.y);
    ask_for_st(A);
   }
 
   public void ask_for_st(int A)
   {
-    s={(Q.p.x-Q.q.y)*(Q.p.x-P.p.x)+(Q.q.x-Q.p.x)*(Q.p.y-P.p.y)}/A;
-    t={(P.p.y-P.q.y)*(Q.p.x-P.p.x)+(P.q.x-P.p.x)*(Q.p.y-P.p.y)}/A;
-
-
+    s=((Q.p.x-Q.q.y)*(Q.p.x-P.p.x)+(Q.q.x-Q.p.x)*(Q.p.y-P.p.y))/A;
+    t=((P.p.y-P.q.y)*(Q.p.x-P.p.x)+(P.q.x-P.p.x)*(Q.p.y-P.p.y))/A;
   }
 
 
   public double judje_cross(double s, double t){
       if(0<=s && s<=1){
-        if((0<=t && t<=1){
+        if(0<=t && t<=1){
           point_x_calc(s,x_p,x_q);
           point_y_calc(s,y_p,y_q);
         }
