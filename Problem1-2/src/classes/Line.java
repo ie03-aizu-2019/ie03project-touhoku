05e0a35172a3d58fd87ce11a057e7ddf489e2ded
Penguin <naoto121jp@yahoo.co.jp>
2019-04-17 10:35:54 +0900

    done 1-2

---
+++ Problem1-2/src/classes/Line.java
@@ -0,0 +1,12 @@
+package classes;
+import classes.*;
+
+public class Line {
+    public Vec P1;
+    public Vec P2;
+
+    public Line (Vec P1, Vec P2) {
+        this.P1 = P1;
+        this.P2 = P2;
+    }
+}
