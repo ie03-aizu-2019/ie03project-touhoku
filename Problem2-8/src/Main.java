4926c430a2e5badbc149d9665bd5458443bab294
Kimitaka Otomo <taka.052376@gmail.com>
2019-05-29 12:18:11 +0900

    2-8途中

--- Problem2-8/src/Main.java
+++ Problem2-8/src/Main.java
@@ -29,14 +29,29 @@ public class Main{
         for (var n=0;n<lineNum;n++){
             for (var i=n+1;i<lineNum;i++) {
                 crossPoints.add(new CrossPoint(lines.get(n),lines.get(i)));
+                crossPoints.get(n).Ask_for_a();
+                if (crossPoints.get(n).Judje_cross()){
+                    crossPoints.get(n).Point_calc();
+                    //points.add(new Vec(crossPoints.get(n).CrossingVec.x,crossPoints.get(n).CrossingVec.y));
+                    lines.add(new Line(lines.get(n).P1, crossPoints.get(n).CrossingVec));
+                    lines.add(new Line(lines.get(n).P2, crossPoints.get(n).CrossingVec));
+                    lines.add(new Line(lines.get(i).P1, crossPoints.get(n).CrossingVec));
+                    lines.add(new Line(lines.get(i).P2, crossPoints.get(n).CrossingVec));
+                    lines.get(n).linestate=false;
+                    lines.get(i).linestate=false;                    
+                }
             }
         }
         
+        for(var n=0;n<crossPoints.size();n++){
+            
+        }
         
-        for (var n=0;n<crossPoints.size();n++) {
-            crossPoints.get(n).Ask_for_a();
-            crossPoints.get(n).Point_calc();
+        for (var n=0;n<lines.size();n++) {
+            /*crossPoints.get(n).Ask_for_a();
             if (crossPoints.get(n).Judje_cross()) {
+                crossPoints.get(n).Point_calc();
+                points.add(new Vec(crossPoints.get(n).CrossingVec.x,crossPoints.get(n).CrossingVec.y));*/
                 System.out.printf("%f,%f\n", crossPoints.get(n).CrossingVec.x,crossPoints.get(n).CrossingVec.y);
             }
         }   
